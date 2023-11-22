3f655e0cc048e237d041e470d92e2cc051c27944
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-22 17:07:20 +0530

    this is my day12 Assignment

---
+++ Assignment_17_1/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_17_1
+{
+    public class BankAccount
+    {
+        readonly long AccountNumber;
+        string AccountHolderName;
+        double Balance;
+
+        public BankAccount(long AccountNumber, string AccountHolderName, double Balance)
+        {
+            this.AccountNumber = AccountNumber;
+            this.AccountHolderName = AccountHolderName;
+            this.Balance = Balance;
+        }
+        public long Account { get { return AccountNumber; } }
+        public string Name { get { return AccountHolderName; } }
+        public double Bal { get { return Balance; } }
+
+        public void Deposit(int depositedamount)
+        {
+            Console.WriteLine($"Deposited Amount: " + depositedamount);
+            Balance = Balance + depositedamount;
+            Console.WriteLine("Balance after Deposit: " + Balance);
+        }
+
+        public void Withdraw(int withdrawamount)
+        {
+            Console.WriteLine("Withrawal Amount: " + withdrawamount);
+            Balance = Balance - withdrawamount;
+            Console.WriteLine("Balance after Withdrawal: " + Balance);
+        }
+    }
+}
---
+++ Assignment_17_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_17_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bank = new BankAccount(543216745378, "Priyanka", 0);
+            Console.WriteLine("Account Details");
+            Console.WriteLine("Account Number: " + bank.Account);
+            Console.WriteLine("Account Holder Name: " + bank.Name);
+            Console.WriteLine("Account Balance: " + bank.Bal);
+            Console.WriteLine();
+            Console.WriteLine("Details After First Deposit");
+            Console.WriteLine("Account Details");
+            Console.WriteLine("Account Number: " + bank.Account);
+            Console.WriteLine("Account Holder Name: " + bank.Name);
+            bank.Deposit(50000);
+            Console.WriteLine();
+            Console.WriteLine("Details after First Withdrawal");
+            Console.WriteLine("Account Details");
+            Console.WriteLine("Account Number: " + bank.Account);
+            Console.WriteLine("Account Holder Name: " + bank.Name);
+            bank.Withdraw(3000);
+            //bank.Deposit(9000);
+            //bank.Deposit(9000);
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
